da67045a40ae90c11d2a00488dc0c13e4a0712fc
SHUAXINDIARY <shuaxinjs@qq.com>
2022-03-03 19:40:30 +0800

    feat: add star btn

--- layouts/BackUp/index.tsx
+++ layouts/BackUp/index.tsx
@@ -1,27 +1,43 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 type BackUpProps = {
-  children?: ReactNode;
-  backNode?: ReactNode;
+    children?: ReactNode;
+    backNode?: ReactNode;
+};
+
+const GotoBtn = () => {
+    return (
+        <Link href="/" passHref>
+            <button className="btn btn-primary">BACKHOME</button>
+        </Link>
+    );
+};
+
+interface JumpBtn {
+    url: string;
+    text: string;
 }
 
-const BackBtn = () => {
-  return (
-    <Link href="/" passHref>
-      <button className="btn btn-primary">BACKHOME</button>
-    </Link>
-  );
+const JumpBtn:FC<JumpBtn> = ({url,text}) => {
+    return (
+        <a href={url} target="_blank" rel="noreferrer">
+            <button className="btn btn-primary">{text}</button>
+        </a>
+    );
 };
 
 const BackUp = ({ children, backNode }: BackUpProps) => {
-  return (
-    <>
-      <div>{children}</div>
-      <div className="absolute right-0 bottom-0 text-red-600 p-3 cursor-pointer">
-        <div>{backNode || BackBtn()}</div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>{children}</div>
+            <div className="absolute right-0 bottom-0 text-red-600 p-3 cursor-pointer flex space-x-4">
+                <div>
+                    <JumpBtn url="https://github.com/SHUAXINDIARY/Toys" text="Star" />
+                </div>
+                <div>{backNode || GotoBtn()}</div>
+            </div>
+        </>
+    );
 };
 export default BackUp;
